eacd0c584f3536961196ba2b6be8b349d976c1a4
krishna-cheashty <krishnacheashty@gmail.com>
2022-01-02 12:07:12 +0600

    added a component

---
+++ src/emon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const emon = () => {
+    return (
+        <div>
+            <h1>ok emon vai
+                
+            </h1>
+        </div>
+    );
+};
+
+export default emon;
