90794c02693d11b1d7999e20b13ac4f36a1944e9
forhadakbar <forhadakbar@yahoo.com>
2022-01-02 00:22:36 -0500

    added principal and teachers

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Principal from '../Principal/Principal';
+import Teachers from '../Teachers/Teachers';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Principal></Principal>
+            <Teachers></Teachers>
         </div>
     );
 };
---
+++ src/Pages/Home/Teachers/Teachers.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import Teacher from '../Teacher/Teacher';
+import "./Teachers.css"
+
+const Teachers = () => {
+
+    const [teachers, setTeachers] = useState([]);
+
+    useEffect(() => {
+        fetch("/FakeData.JSON")
+            .then((res) => res.json())
+            .then((data) => {
+                setTeachers(data);
+            });
+    }, []);
+
+
+    return (
+        <div>
+
+            <section className='client-feedback d-flex align-items-center justify-content-center my-5'>
+                <div className='container mb-5 mt-3'>
+                    <h3 className='text-center mb-5' style={{ fontSize: '36px', fontWeight: '600' }}>
+                        <span style={{ color: '#171B4E' }}>MEET </span>
+                        <span className="text-success">OUR TEACHERS</span>
+                        <h4 className="mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry</h4>
+                    </h3>
+
+                    <div className='row my-5'>
+                        {teachers.map((teacher) => (
+                            <Teacher key={teacher.id} teacher={teacher} />
+                        ))}
+                    </div>
+
+
+
+
+                </div>
+            </section>
+
+        </div>
+    );
+};
+
+export default Teachers;
