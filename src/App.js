0a020cf772ad0cb75ef68007cb4a9849eaa398f2
krishna-cheashty <krishnacheashty@gmail.com>
2022-01-03 13:13:09 +0600

    added ruducer trank

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NotFound from './Pages/NotFound/NotFound';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
+
 function App() {
   return (
     <div>
